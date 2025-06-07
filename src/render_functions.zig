122de5402b8f9deb53e4c5725707e446827d76d3
ssteinbach <ssteinbach@users.noreply.github.com>
2025-06-07 09:56:33 -0700

    implement image 2

--- src/render_functions.zig
+++ src/render_functions.zig
@@ -2,6 +2,7 @@
 
 const std = @import("std");
 const raytrace = @import("root.zig");
+const ray = @import("ray.zig");
 const vector = @import("vector.zig");
 const comath_wrapper = @import("comath_wrapper.zig");
 
@@ -15,6 +16,7 @@ pub const render_fn = *const fn(
 /// fun way of cataloging the history of the renders that this project can make
 pub const CHECKPOINTS:[]const render_fn = &[_]render_fn{
     image_1,
+    image_2.render,
 };
 
 /// main render function - this is where the "main" code from Raytracing in a
@@ -65,3 +67,115 @@ pub fn image_1(
     }
 }
 
+const image_2 = struct {
+    pub fn ray_color(
+        r: ray.Ray,
+    ) vector.Color3f
+    {
+        const unit_dir = r.dir.unit_vector();
+        const a = 0.5 * (unit_dir.y + 1.0);
+
+        return comath_wrapper.lerp(
+            a,
+            vector.Color3f.init(1.0),
+            vector.Color3f.init([_]f32{0.5, 0.7, 1.0}),
+        );
+    }
+
+    const BaseType = vector.V3f.BaseType;
+
+
+    pub fn render(
+        _: std.mem.Allocator,
+        img: *raytrace.Image_rgba_u8,
+        _: usize,
+    ) void
+    {
+        const aspect_ratio:vector.V3f.BaseType = 16.0 / 9.0;
+        const image_width:usize = 400;
+
+        const image_height:usize = @intFromFloat(
+            @max(1.0, @as(BaseType, @floatFromInt(image_width)) / aspect_ratio)
+        );
+
+        const camera = struct {
+            const focal_length : BaseType = 1.0;
+            const viewport_height : BaseType = 2.0;
+            const viewport_width : BaseType = (
+                (viewport_height * image_width) / image_height
+            );
+
+            // camera at the origin
+            const center = vector.Point3f.init(0);
+        };
+
+        const viewport_u: vector.V3f = .{ 
+            .x = camera.viewport_width,
+            .y = 0,
+            .z = 0,
+        };
+        const viewport_v: vector.V3f = .{ 
+            .x = 0,
+            .y = -camera.viewport_height,
+            .z = 0,
+        };
+
+        const pixel_delta_u = viewport_u.div(image_width);
+        const pixel_delta_v = viewport_v.div(image_height);
+
+        const viewport_upper_left = comath_wrapper.eval(
+            "camera_center - v_fl - (v_u/2) - (v_v/2)",
+            .{
+                .camera_center = camera.center,
+                .v_fl = vector.V3f.init([_]BaseType{0,0, camera.focal_length}),
+                .v_u = viewport_u,
+                .v_v = viewport_v,
+            },
+        );
+
+        const pixel00_loc = comath_wrapper.eval(
+            "v_ul + (pdu + pdv) * 0.5",
+            .{
+                .v_ul = viewport_upper_left,
+                .pdu = pixel_delta_u,
+                .pdv = pixel_delta_v,
+            },
+        );
+
+        var j:usize = 0;
+        while (j < image_height)
+            : (j+=1)
+        {
+            var i:usize = 0;
+            while (i < image_width)
+                : (i+=1)
+            {
+                const pixel_center = comath_wrapper.eval(
+                    "p00 + (p_du*i) + (p_dv * j)",
+                    .{ 
+                        .p00 = pixel00_loc,
+                        .p_du = pixel_delta_u,
+                        .p_dv = pixel_delta_v,
+                        .i = i,
+                        .j = j,
+                    },
+                );
+
+                const r = ray.Ray{
+                    .origin = camera.center,
+                    .dir = pixel_center.sub(camera.center),
+                };
+
+                // scale and convert to output pixel format
+                const pixel_color = ray_color(r).mul(255.999).as(u8);
+
+                var pixel = img.pixel(i, j);
+
+                pixel[0] = pixel_color.x;
+                pixel[1] = pixel_color.y;
+                pixel[2] = pixel_color.z;
+                pixel[3] = 255;
+            }
+        }
+    }
+};
