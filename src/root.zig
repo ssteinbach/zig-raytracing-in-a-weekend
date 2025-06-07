b25006bb801d2ca5568d7e89d84b6778ed76f0b3
ssteinbach <ssteinbach@users.noreply.github.com>
2025-06-07 07:41:43 -0700

    update docs

--- src/root.zig
+++ src/root.zig
@@ -91,14 +91,17 @@ test "comath integration"
         expr: []const u8,
         result: vector.V3f,
     }{
+        // addition
         .{
             .expr = "v1 + v2",
             .result = .{ .x = 1.5, .y = 3, .z = 4 } 
         },
+        // subtraction
         .{
             .expr = "v1 - v2",
             .result = .{ .x = 0.5, .y = 1, .z = 2 } 
         },
+        // cross
         .{
             .expr = "v1 ^ v2",
             .result = .{ .x = -1, .y = 0.5, .z = 0 } 
