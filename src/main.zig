50853d58fe3044614dfd10e43070b8a8ee98cb54
ssteinbach <ssteinbach@users.noreply.github.com>
2025-06-07 07:26:10 -0700

    cleanup

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,9 @@
 
 const std = @import("std");
 
-pub fn main() !void {
+pub fn main(
+) !void 
+{
     std.debug.print(
         "will call raytrace.render() and write an image to disk.  "
         ++ "For now use `zig build viewer` to open the interactive viewer.\n",
