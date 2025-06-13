aa8310dc49395d3104c4cbbda98695359940d995
ssteinbach <ssteinbach@users.noreply.github.com>
2025-06-12 21:26:32 -0700

    image 10 (diffuse reflection)

--- src/render_functions.zig
+++ src/render_functions.zig
@@ -29,6 +29,7 @@ pub const CHECKPOINTS:[]const render_fn = &[_]render_fn{
     image_7.render,
     image_8.render,
     image_9.render,
+    image_10.render,
 };
 
 pub const CHECKPOINT_NAMES = [_][:0]const u8{
@@ -41,7 +42,8 @@ pub const CHECKPOINT_NAMES = [_][:0]const u8{
     "Image 6 (Antialiasing)",
     "Image 7 (Diffuse Sampling)",
     "Image 8 (Limited bounces)",
-    "Image 9 (Limited bounces)",
+    "Image 9 (No Shadow Acne)",
+    "Image 10 (Correct Lambertian response)",
 };
 
 pub const RENDERERS = [_]Renderer{
@@ -55,6 +57,7 @@ pub const RENDERERS = [_]Renderer{
     Renderer.init(image_7),
     Renderer.init(image_8),
     Renderer.init(image_9),
+    Renderer.init(image_10),
 };
 
 fn maybe_decl(
@@ -1877,6 +1880,296 @@ const image_9 = struct {
                 |hitrec|
             {
                 const dir = utils.random_on_hemisphere(hitrec.normal);
+                return ray_color(
+                    .{ .origin = hitrec.p, .dir = dir},
+                    depth - 1,
+                    world,
+                ).mul(0.5);
+            }
+
+            const a = 0.5 * (r.dir.unit_vector().y + 1.0);
+
+            return comath_wrapper.lerp(
+                a,
+                vector.Color3f.init(1.0),
+                vector.Color3f.init([_]f32{0.5, 0.7, 1.0}),
+            );
+        }
+
+        pub fn render(
+            self: @This(),
+            world: HittableSlice,
+            img: *raytrace.Image_rgba_u8,
+        ) void
+        {
+            var j:usize = 0;
+            while (j < self.image_height)
+                : (j+=1)
+            {
+                var i:usize = 0;
+                while (i < self.image_width)
+                    : (i+=1)
+                {
+                    var pixel_color = vector.Color3f.init(0);
+
+                    var sample: usize = 0;
+                    while (sample < samples_per_pixel)
+                        : (sample += 1)
+                    {
+                        const r = self.get_ray(
+                            @floatFromInt(i),
+                            @floatFromInt(j)
+                        );
+
+                        // scale and convert to output pixel foormat
+                        pixel_color = pixel_color.add(ray_color(
+                                r,
+                                max_depth,
+                                world,
+                            )
+                        );
+                    }
+                    img.write_pixel(i,j, pixel_color.mul(pixel_sample_scale));
+
+                }
+            }
+
+        }
+
+        fn get_ray(
+            self: @This(),
+            i: BaseType,
+            j: BaseType,
+        ) ray.Ray 
+        {
+            const offset = sample_square();
+            const pixel_sample = comath_wrapper.eval(
+                "p00 + (pdu*(i + o_x)) + (pdv*(j+o_y))",
+                .{
+                    .p00 = self.pixel00_loc,
+                    .i = i,
+                    .j = j,
+                    .o_x = offset.x,
+                    .o_y = offset.y,
+                    .pdu = self.pixel_delta_u,
+                    .pdv = self.pixel_delta_v,
+                },
+            );
+
+            return .{
+                .origin = self.center,
+                .dir = pixel_sample.sub(self.center),
+            };
+        }
+
+        fn sample_square() vector.V3f
+        {
+            return .{
+                .x = utils.rnd_num(BaseType) - 0.5,
+                .y = utils.rnd_num(BaseType) - 0.5,
+                .z = 0
+            };
+        }
+    };
+
+    pub const State = struct {
+        world: HittableSlice = undefined,
+        camera: Camera = undefined,
+        allocator: std.mem.Allocator,
+
+        pub fn init(
+            allocator: std.mem.Allocator,
+            img: *raytrace.Image_rgba_u8,
+        ) State
+        {
+            // build the world
+            var world = HittableList.init(allocator);
+            defer world.deinit();
+
+            world.append(
+                Hittable.init(
+                    Sphere{
+                        .center_worldspace = vector.V3f.init_3(0,0,-1),
+                        .radius = 0.5,
+                    }
+                )
+            ) catch @panic("OOM!");
+            world.append(
+                Hittable.init(
+                    Sphere{
+                        .center_worldspace = vector.V3f.init_3(0, -100.5,-1),
+                        .radius = 100,
+                    }
+                )
+            ) catch @panic("OOM!");
+
+            const camera = Camera.init(
+                1.0,
+                // camera at the origin
+                vector.Point3f.init(0),
+                img,
+            );
+
+            return .{
+                .camera = camera,
+                .world =  world.toOwnedSlice() catch @panic("OOM"),
+                .allocator = allocator,
+            };
+        }
+
+        pub fn render(
+            self: @This(),
+            img: *raytrace.Image_rgba_u8,
+        ) void
+        {
+            self.camera.render(self.world, img);
+        }
+
+        pub fn deinit(
+            self: @This(),
+        ) void
+        {
+            self.allocator.free(self.world);
+        }
+    };
+    var state: ?State = null;
+
+    pub fn render(
+        allocator: std.mem.Allocator,
+        img: *raytrace.Image_rgba_u8,
+        _: usize,
+    ) void
+    {
+        if (state == null)
+        {
+            state = State.init(allocator, img);
+        }
+
+        state.?.render(img);
+    }
+
+    pub fn init(
+       allocator: std.mem.Allocator,
+       img: *raytrace.Image_rgba_u8,
+    ) void
+    {
+        state = State.init(allocator, img);
+    }
+
+    pub fn deinit(
+    ) void
+    {
+        state.?.deinit();
+        state = null;
+    }
+};
+
+const image_10 = struct {
+    const Camera = struct {
+        /// focal length of the camera (thin lens) of 1.0
+        focal_length : BaseType = 1.0,
+        /// camera at the origin by default
+        center :vector.V3f = vector.Point3f.init(0),
+
+        image_width: usize,
+        image_height: usize,
+
+        pixel00_loc: vector.Point3f,
+        pixel_delta_u: vector.V3f,
+        pixel_delta_v: vector.V3f,
+
+        const samples_per_pixel:usize = 10;
+        const pixel_sample_scale:BaseType = (
+            1.0/@as(BaseType, @floatFromInt(samples_per_pixel))
+        );
+
+        // maximum number of ray bounces
+        const max_depth = 10;
+
+        pub fn init(
+            focal_length: BaseType,
+            center: vector.V3f,
+            img: *raytrace.Image_rgba_u8,
+        ) @This()
+        {
+            const aspect_ratio:vector.V3f.BaseType = @floatFromInt(img.width / img.height);
+            const image_width:usize = img.width;
+            const image_height:usize = @intFromFloat(
+                @max(1.0, @as(BaseType, @floatFromInt(image_width)) / aspect_ratio)
+            );
+
+            const viewport_height : BaseType = 2.0;
+            const viewport_width : BaseType = (
+                (
+                 viewport_height 
+                 * @as(BaseType, @floatFromInt(image_width))) / @as(BaseType, @floatFromInt(image_height))
+            );
+
+            const viewport_u: vector.V3f = .{ 
+                .x = viewport_width,
+                .y = 0,
+                .z = 0,
+            };
+            const viewport_v: vector.V3f = .{ 
+                .x = 0,
+                .y = -viewport_height,
+                .z = 0,
+            };
+
+            const viewport_upper_left = comath_wrapper.eval(
+                "camera_center - v_fl - (v_u/2) - (v_v/2)",
+                .{
+                    .camera_center = center,
+                    .v_fl = vector.V3f.init([_]BaseType{0,0, focal_length}),
+                    .v_u = viewport_u,
+                    .v_v = viewport_v,
+                },
+                );
+
+            const pixel_delta_u = viewport_u.div(image_width);
+            const pixel_delta_v = viewport_v.div(image_height);
+
+            return .{
+                .center = center,
+                .focal_length = focal_length,
+                .image_width = image_width,
+                .image_height = image_height,
+                .pixel_delta_u = pixel_delta_u,
+                .pixel_delta_v = pixel_delta_v,
+                .pixel00_loc = comath_wrapper.eval(
+                    "v_ul + (pdu + pdv) * 0.5",
+                    .{
+                        .v_ul = viewport_upper_left,
+                        .pdu = pixel_delta_u,
+                        .pdv = pixel_delta_v,
+                    },
+                ),
+            };
+        }
+
+        pub fn ray_color(
+            r: ray.Ray,
+            depth: i16,
+            world: HittableSlice,
+        ) vector.Color3f
+        {
+            if (depth <= 0)
+            {
+                return vector.Color3f.ZERO;
+            }
+
+            if (
+                hit_world(
+                    .{ 
+                        .world = world,
+                        .r = r,
+                        .interval = utils.Interval.EPS_TO_INF,
+                    }
+                ) 
+            )
+                |hitrec|
+            {
+                const dir = hitrec.normal.add(utils.random_unit_vector());
                 return ray_color(
                     .{ .origin = hitrec.p, .dir = dir},
                     depth - 1,
