db26b47bc5089cc433dc15aa7b1d383dfe6152f3
ssteinbach <ssteinbach@users.noreply.github.com>
2025-07-09 22:03:11 -0700

    refactor build.zig and add other exe

--- build.zig
+++ build.zig
@@ -1,5 +1,71 @@
 const std = @import("std");
 
+const BuildState = struct {
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    dep_ziis: *std.Build.Dependency,
+    lib_mod: *std.Build.Module,
+    check_step: *std.Build.Step,
+    test_step: *std.Build.Step,
+};
+
+pub fn executable(
+    b: *std.Build,
+    name: []const u8,
+    main_filepath: []const u8,
+    state: BuildState,
+) void
+{
+        const exe_mod = b.createModule(
+            .{
+                .root_source_file = b.path(main_filepath),
+                .target = state.target,
+                .optimize = state.optimize,
+            }
+        );
+        exe_mod.addImport(
+            "zgui_cimgui_implot_sokol",
+            state.dep_ziis.module("zgui_cimgui_implot_sokol")
+        );
+
+        exe_mod.addImport("raytrace", state.lib_mod);
+
+        const exe = b.addExecutable(
+            .{
+                .name = "viewer",
+                .root_module = exe_mod,
+            }
+        );
+        state.check_step.dependOn(&exe.step);
+
+        b.installArtifact(exe);
+
+        const run_cmd = b.addRunArtifact(exe);
+
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        if (b.args) 
+            |args| 
+        {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step(
+            name,
+            "Run the viewer"
+        );
+        run_step.dependOn(&run_cmd.step);
+
+        const exe_unit_tests = b.addTest(
+            .{
+                .root_module = exe_mod,
+            }
+        );
+
+        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+        state.test_step.dependOn(&run_exe_unit_tests.step);
+}
+
 pub fn build(
     b: *std.Build
 ) void 
@@ -101,63 +167,38 @@ pub fn build(
         test_step.dependOn(&run_exe_unit_tests.step);
     }
 
+
+    const state = BuildState{
+        .target = target,
+        .optimize = optimize,
+        .dep_ziis = b.dependency(
+            "zgui_cimgui_implot_sokol",
+            .{
+                .optimize = optimize,
+                .target = target,
+            }
+        ),
+        .lib_mod = lib_mod,
+        .check_step = check_step,
+        .test_step = test_step,
+    };
+
     // gui viewer
     {
-        const ziis_dep = b.dependency(
-            "zgui_cimgui_implot_sokol",
-            .{
-                .optimize = optimize,
-                .target = target,
-            }
-        );
-
-        const exe_mod = b.createModule(
-            .{
-                .root_source_file = b.path("src/viewer.zig"),
-                .target = target,
-                .optimize = optimize,
-            }
-        );
-        exe_mod.addImport(
-            "zgui_cimgui_implot_sokol",
-            ziis_dep.module("zgui_cimgui_implot_sokol")
-        );
-
-        exe_mod.addImport("raytrace", lib_mod);
-
-        const exe = b.addExecutable(
-            .{
-                .name = "viewer",
-                .root_module = exe_mod,
-            }
-        );
-        check_step.dependOn(&exe.step);
-
-        b.installArtifact(exe);
-
-        const run_cmd = b.addRunArtifact(exe);
-
-        run_cmd.step.dependOn(b.getInstallStep());
-
-        if (b.args) 
-            |args| 
-        {
-            run_cmd.addArgs(args);
-        }
-
-        const run_step = b.step(
+        executable(
+            b,
             "viewer",
-            "Run the viewer"
+            "src/viewer.zig",
+            state
         );
-        run_step.dependOn(&run_cmd.step);
+    }
 
-        const exe_unit_tests = b.addTest(
-            .{
-                .root_module = exe_mod,
-            }
+    {
+        executable(
+            b,
+            "viewer3d",
+            "src/viewer3d.zig",
+            state
         );
-
-        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-        test_step.dependOn(&run_exe_unit_tests.step);
     }
 }
