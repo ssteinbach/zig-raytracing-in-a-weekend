3c37ddec5caff30b82ed31abc5dcfab45a9b9749
ssteinbach <ssteinbach@users.noreply.github.com>
2025-12-01 11:27:44 -0800

    reformat build.zig

--- build.zig
+++ build.zig
@@ -16,54 +16,54 @@ pub fn executable(
     state: BuildState,
 ) void
 {
-        const exe_mod = b.createModule(
-            .{
-                .root_source_file = b.path(main_filepath),
-                .target = state.target,
-                .optimize = state.optimize,
-            }
-        );
-        exe_mod.addImport(
-            "zgui_cimgui_implot_sokol",
-            state.dep_ziis.module("zgui_cimgui_implot_sokol")
-        );
-
-        exe_mod.addImport("raytrace", state.lib_mod);
-
-        const exe = b.addExecutable(
-            .{
-                .name = name,
-                .root_module = exe_mod,
-            }
-        );
-        state.check_step.dependOn(&exe.step);
-
-        b.installArtifact(exe);
-
-        const run_cmd = b.addRunArtifact(exe);
-
-        run_cmd.step.dependOn(b.getInstallStep());
-
-        if (b.args) 
-            |args| 
-        {
-            run_cmd.addArgs(args);
+    const exe_mod = b.createModule(
+        .{
+            .root_source_file = b.path(main_filepath),
+            .target = state.target,
+            .optimize = state.optimize,
         }
+    );
+    exe_mod.addImport(
+        "zgui_cimgui_implot_sokol",
+        state.dep_ziis.module("zgui_cimgui_implot_sokol")
+    );
 
-        const run_step = b.step(
-            name,
-            "Run the viewer"
-        );
-        run_step.dependOn(&run_cmd.step);
+    exe_mod.addImport("raytrace", state.lib_mod);
 
-        const exe_unit_tests = b.addTest(
-            .{
-                .root_module = exe_mod,
-            }
-        );
+    const exe = b.addExecutable(
+        .{
+            .name = name,
+            .root_module = exe_mod,
+        }
+    );
+    state.check_step.dependOn(&exe.step);
 
-        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-        state.test_step.dependOn(&run_exe_unit_tests.step);
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) 
+        |args| 
+    {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step(
+        name,
+        "Run the viewer"
+    );
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_unit_tests = b.addTest(
+        .{
+            .root_module = exe_mod,
+        }
+    );
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    state.test_step.dependOn(&run_exe_unit_tests.step);
 }
 
 pub fn build(
